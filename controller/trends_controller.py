618fcd03a98a4d4b5edd35de7fced94da0c54651
Luis_Urdaneta <luisurdanetawhite@outlook.com>
2023-11-20 23:39:53 -0500

    Endpoint for avg shipment speed for a facility monthly

--- controller/trends_controller.py
+++ controller/trends_controller.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter
 from starlette.exceptions import HTTPException
 from starlette.responses import JSONResponse
 
-from service.trends_service import tmrs_completed_service, tmrs_received_service
+from service.trends_service import tmrs_completed_service, tmrs_received_service, shipment_speed_service
 
 router = APIRouter()
 
@@ -31,6 +31,25 @@ async def tmrs_received(facility_id: str = ""):
         return JSONResponse(
             content={
                 "received_current_month": current_month_received,
+                "change": delta
+            }, status_code=200)
+    except HTTPException as http_exception:
+        print(http_exception.detail)
+        if http_exception.status_code == 404:
+            return JSONResponse(content={"error": "No results found"}, status_code=404)
+        else:
+            return JSONResponse(content={"error": str("Internal Server error")}, status_code=500)
+    except Exception as e:
+        return JSONResponse(content={"error": str("Internal Server Error")}, status_code=500)
+
+
+@router.get("/shipmentSpeed")
+async def shipment_speed(facility_id: str = ""):
+    try:
+        current_month_speed, delta = await shipment_speed_service(facility_id)
+        return JSONResponse(
+            content={
+                "shipment_speed": current_month_speed,
                 "change": delta
             }, status_code=200)
     except HTTPException as http_exception:
--- service/trends_service.py
+++ service/trends_service.py
@@ -47,3 +47,43 @@ async def tmrs_received_service(facility_id: str = ""):
 
     except HTTPException as http_exception:
         raise http_exception
+
+
+async def shipment_speed_service(facility_id: str = ""):
+    try:
+        tmrs = await find_all_tmrs(facility_id)
+
+        # Get the current month and the first day of the current month
+        current_month = datetime.now().month
+
+        # Filter records for the current month and the previous month
+        current_month_records = list(filter(lambda tmr: tmr['date_received'] and tmr['add'] and tmr['add'].month == current_month, tmrs))
+        last_month_records = list(filter(lambda tmr: tmr['date_received'] and tmr['add'] and tmr['add'].month == current_month - 1, tmrs))
+
+        print(current_month_records)
+        # Calculate shipment speed in days for the current month
+        current_month_speeds = []
+        for tmr in current_month_records:
+            speed_days = (tmr['add'] - tmr['date_received']).days
+            current_month_speeds.append(speed_days)
+
+        # Calculate average shipment speed for the current month
+        average_speed_current_month = sum(current_month_speeds) / len(
+            current_month_speeds) if current_month_speeds else 0
+
+        # Calculate average shipment speed for the previous month
+        last_month_speeds = []
+        for tmr in last_month_records:
+            if tmr['add'] and tmr['date_received']:
+                speed_days = (tmr['add'] - tmr['date_received']).days
+                last_month_speeds.append(speed_days)
+
+        average_speed_last_month = sum(last_month_speeds) / len(last_month_speeds) if last_month_speeds else 0
+
+        # Calculate change compared to the previous month
+        delta = ((average_speed_current_month - average_speed_last_month) / average_speed_last_month) * 100 if average_speed_last_month != 0 else 0
+
+        return average_speed_current_month, delta
+
+    except HTTPException as http_exception:
+        raise http_exception
