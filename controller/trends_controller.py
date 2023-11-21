ec58d33e4f4aaf8e2648392d7de81837aa3f9952
Luis_Urdaneta <luisurdanetawhite@outlook.com>
2023-11-20 23:04:03 -0500

    endpoint for getting all tmrs received in a month by facility and change

--- controller/trends_controller.py
+++ controller/trends_controller.py
@@ -2,17 +2,35 @@ from fastapi import APIRouter
 from starlette.exceptions import HTTPException
 from starlette.responses import JSONResponse
 
-from service.trends_service import tmrs_completed_service
+from service.trends_service import tmrs_completed_service, tmrs_received_service
 
 router = APIRouter()
 
 @router.get("/tmrsCompleted")
 async def tmrs_completed(facility_id: str = ""):
     try:
-        current_month_completed, delta =  await tmrs_completed_service(facility_id)
+        current_month_completed, delta = await tmrs_completed_service(facility_id)
         return JSONResponse(
             content={
                 "completed_current_month": current_month_completed,
+                "change": delta
+            }, status_code=200)
+    except HTTPException as http_exception:
+        print(http_exception.detail)
+        if http_exception.status_code == 404:
+            return JSONResponse(content={"error": "No results found"}, status_code=404)
+        else:
+            return JSONResponse(content={"error": str("Internal Server error")}, status_code=500)
+    except Exception as e:
+        return JSONResponse(content={"error": str("Internal Server Error")}, status_code=500)
+
+@router.get("/tmrsReceived")
+async def tmrs_received(facility_id: str = ""):
+    try:
+        current_month_received, delta = await tmrs_received_service(facility_id)
+        return JSONResponse(
+            content={
+                "received_current_month": current_month_received,
                 "change": delta
             }, status_code=200)
     except HTTPException as http_exception:
